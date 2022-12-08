b7edcbe41a693c5a4665d393580401c1b5f7cd8d
Aleksey Usynin <fesskagf@yandex.ru>
2022-12-08 12:12:04 +0400

    Исправления по замечаниям от Ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,39 @@
 const profileEditButton = document.querySelector(".profile__edit-button");
 const popup = document.querySelector(".popup");
 const popupButtonClose = document.querySelector(".popup__button-close");
-
+const popupContainer = document.querySelector(".popup__container");
 const popupForm = document.querySelector(".popup__form");
 const popupInputName = document.querySelector(".popup__input-name");
 const popupInputJob = document.querySelector(".popup__input-job");
-
 const profileTitle = document.querySelector(".profile__title");
 const profileSubtitle = document.querySelector(".profile__subtitle");
 
-// открываем попап по кнопке
 function openPopup() {
-  popup.classList.add("popup_opened");
-  popupInputName.value = profileTitle.textContent;
-  popupInputJob.value = profileSubtitle.textContent;
+  //функция открывающая popup
+  popup.classList.add("popup_opened"); // добавляем клас открывающий popup
+  popupInputName.value = profileTitle.textContent; //передаем в popup текст имени из header
+  popupInputJob.value = profileSubtitle.textContent; //передаетм в popup текс работы из header
 }
 
-// закрываем попам по крестику
 function closePopup() {
-  popup.classList.remove("popup_opened");
+  //функция закрывающая popup
+  popup.classList.remove("popup_opened"); // удаляем класс что бы убрать popup
 }
-
-// закрываем попап по клику на поле вне попап контейнера
-popup.addEventListener("click", closePopup);
-document
-  .querySelector(".popup__container")
-  .addEventListener("click", function (event) {
-    event.stopPropagation();
-  });
 
 function handleFormSubmit(evt) {
-  evt.preventDefault();
-  profileTitle.textContent = popupInputName.value;
-  profileSubtitle.textContent = popupInputJob.value;
-  popup.classList.remove("popup_opened");
+  //функция передающая текст из popup в header
+  evt.preventDefault(); //отменяем стандартное поведение submit
+  profileTitle.textContent = popupInputName.value; //передаем в header текст имени из popup
+  profileSubtitle.textContent = popupInputJob.value; //передаетм в header текс работы из popup
+  popup.classList.remove("popup_opened"); // удаляем класс что бы убрать popup
 }
 
+function notSurface(event) {
+  event.stopPropagation(); //отменяет всплытие
+}
+
+popupContainer.addEventListener("click", notSurface);
+popup.addEventListener("click", closePopup);
 profileEditButton.addEventListener("click", openPopup);
 popupButtonClose.addEventListener("click", closePopup);
 popupForm.addEventListener("submit", handleFormSubmit);
